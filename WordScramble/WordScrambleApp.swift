88bf8ad6ae9cc714c5e45e7f7328b2fad938cd10
KiranJungGurung <kirangurung394@gmail.com>
2022-12-30 11:59:06 +0545

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kiran Gurung on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
